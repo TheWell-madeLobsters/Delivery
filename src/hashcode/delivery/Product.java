b7a249bf7aa6d4464f723d4478c73f366cc37d40
Marco Terrinoni <marco.terrinoni90@gmail.com>
2016-02-15 23:15:52 +0100

    Licese headers update

--- src/hashcode/delivery/Product.java
+++ src/hashcode/delivery/Product.java
@@ -1,7 +1,7 @@
 /*
- * The MIT License
+ * The MIT License (MIT)
  *
- * Copyright 2016 The Well-made Lobsters.
+ * Copyright (c) 2016 The Well-made Lobsters.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
